3ec759197fbe0952b43ce4d1fafaca326348b986
StevenNguyen0501 <trungtin92tp1@gmail.com>
2023-12-12 17:18:44 +0700

    [Create] Add file path to refactor code

--- tests/visual-test.py
+++ tests/visual-test.py
@@ -17,24 +17,27 @@ model_names = [
 ]
 detector_backends = ["opencv", "ssd", "dlib", "mtcnn", "retinaface"]
 
+img_path_1 = "/Users/stevennguyen/Documents/VNEXT/VNEXT-Projects/VNEXT-FaceRecognition/deepface/tests/dataset/img1.jpg"
+img_path_2 = "/Users/stevennguyen/Documents/VNEXT/VNEXT-Projects/VNEXT-FaceRecognition/deepface/tests/dataset/img2.jpg"
+
 # verification
 for model_name in model_names:
     obj = DeepFace.verify(
-        img1_path="dataset/img1.jpg", img2_path="dataset/img2.jpg", model_name=model_name
+        img1_path=img_path_1, img2_path=img_path_2, model_name=model_name
     )
     logger.info(obj)
     logger.info("---------------------")
 
 # represent
 for model_name in model_names:
-    embedding_objs = DeepFace.represent(img_path="dataset/img1.jpg", model_name=model_name)
+    embedding_objs = DeepFace.represent(img_path=img_path_1, model_name=model_name)
     for embedding_obj in embedding_objs:
         embedding = embedding_obj["embedding"]
         logger.info(f"{model_name} produced {len(embedding)}D vector")
 
 # find
 dfs = DeepFace.find(
-    img_path="dataset/img1.jpg", db_path="dataset", model_name="Facenet", detector_backend="mtcnn"
+    img_path=img_path_1, db_path="tests/dataset", model_name="Facenet", detector_backend="mtcnn"
 )
 for df in dfs:
     logger.info(df)
@@ -42,7 +45,7 @@ for df in dfs:
 # extract faces
 for detector_backend in detector_backends:
     face_objs = DeepFace.extract_faces(
-        img_path="dataset/img1.jpg", detector_backend=detector_backend
+        img_path=img_path_1, detector_backend=detector_backend
     )
     for face_obj in face_objs:
         face = face_obj["face"]
